94b7dc03e33f16aafbbb32b07ed26d8f0a8a615f
Ashish Nanotkar <nanotkarashish@users.noreply.github.com>
2017-10-04 20:46:47 +0530

    adding prod db config

---
+++ config/db-prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    localUrl : 'mongodb://spring:spring@prod-shard-00-00-taygj.mongodb.net:27017,prod-shard-00-01-taygj.mongodb.net:27017,prod-shard-00-02-taygj.mongodb.net:27017/test?ssl=true&replicaSet=prod-shard-0&authSource=admin',
+    remoteUrl: 'mongodb://localhost/meanstacktutorials'
+};
