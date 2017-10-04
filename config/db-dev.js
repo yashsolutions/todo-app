99fbf162868a6634f56428ec02893e6be49da1ac
Ashish Nanotkar <nanotkarashish@users.noreply.github.com>
2017-10-04 20:44:43 +0530

    adding dev db config

---
+++ config/db-dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    localUrl : 'mongodb://spring:spring@dev-shard-00-00-qbcgw.mongodb.net:27017,dev-shard-00-01-qbcgw.mongodb.net:27017,dev-shard-00-02-qbcgw.mongodb.net:27017/test?ssl=true&replicaSet=dev-shard-0&authSource=admin',
+    remoteUrl: 'mongodb://localhost/meanstacktutorials'
+};
