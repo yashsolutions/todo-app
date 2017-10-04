e8e8f3163bfbd739e34476914ebe5a1940c8ab2f
Ashish Nanotkar <nanotkarashish@users.noreply.github.com>
2017-10-04 20:45:53 +0530

    adding stage db config

---
+++ config/db-stage.js
@@ -0,0 +1,4 @@
+module.exports = {
+    localUrl : 'mongodb://spring:spring@stage-shard-00-00-hlozm.mongodb.net:27017,stage-shard-00-01-hlozm.mongodb.net:27017,stage-shard-00-02-hlozm.mongodb.net:27017/test?ssl=true&replicaSet=stage-shard-0&authSource=admin',
+    remoteUrl: 'mongodb://localhost/meanstacktutorials'
+};
